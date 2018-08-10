44dc48ed10edf83ca9670d02a17b6bc3a51e4fb0
Chris Duxbury <dux.chris@gmail.com>
2018-08-09 22:36:03 -0400

    Replace jasmine with jasmine-node. Remove report helpers.

--- spec/support/helpers/reporters.js
+++
@@ -1,5 +0,0 @@
-const Jasmine = require('jasmine');
-const SpecReporter = require('jasmine-spec-reporter');
-let jasmine = new Jasmine();
-let reporter = new SpecReporter();
-jasmine.addReporter(reporter);
