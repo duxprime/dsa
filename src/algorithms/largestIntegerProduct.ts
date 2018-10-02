94964deddb03b48a0193abd96210b6d9dce1f523
Chris Duxbury <chris.duxbury@onbase.com>
2018-10-02 16:12:02 -0400

    Remove unnecessary variable.

--- src/algorithms/largestIntegerProduct.ts
+++ src/algorithms/largestIntegerProduct.ts
@@ -2,14 +2,13 @@
  * Mathematically finds the largest integer in a product.
  */
 export function getLargestInProductModulus(num1:number, num2:number){
-    const product = num1 * num2;
     const factor = 10;
+    let product = num1 * num2;
     let largest = 0;
-    let remaining = product;
 
-    while(remaining > 0){
-        let current  = remaining % factor;
-        remaining = Math.floor(remaining/factor);
+    while(product > 0){
+        let current  = product % factor;
+        product = Math.floor(product/factor);
         if(current > largest){
             largest = current;
         }
@@ -21,7 +20,7 @@ export function getLargestInProductModulus(num1:number, num2:number){
 export function getLargestInProductString(num1:number, num2:number){
     const productParts = (num1 * num2).toString().split('');
     let largest = 0;
-    
+
     while(productParts.length > 0){
         let current = +productParts.pop();
         if(current > largest){
