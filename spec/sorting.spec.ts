9544d4f0c8c225596a4846bae8a178e49487cf75
Chris Duxbury <chris.duxbury@onbase.com>
2018-10-03 09:02:54 -0400

    Rename merge sort file.

--- spec/sorting.spec.ts
+++ spec/sorting.spec.ts
@@ -1,34 +1,35 @@
-import { mergeSortRecursive } from '../src/sorting/merge-sort-recursive';
+import { mergeSortRecursive } from 'sorting/merge-sort';
 import "jasmine";
 
 describe('Sorting tests', () => {
-    describe('#Merge sort', () => {
-        const items = [
-            5,
-            1,
-            9,
-            4,
-            2,
-            7,
-            6,
-            6,
-            3,
-            8
-        ];
-        it('should correctly sort', () => {
-            let sorted = [
-                1,
-                2,
-                3,
-                4,
-                5,
-                6,
-                6,
-                7,
-                8,
-                9
-            ];
+    const items = [
+        5,
+        1,
+        9,
+        4,
+        2,
+        7,
+        6,
+        6,
+        3,
+        8
+    ];
 
+    const sorted = [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        6,
+        7,
+        8,
+        9
+    ];
+
+    describe('#Merge sort', () => {
+        it('should correctly sort', () => {
             expect(mergeSortRecursive(items)).toEqual(sorted);
         });
     });
